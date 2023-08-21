14d89c2058d5dbc59e6b2f6c96dcb2c99b97dc02
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-21 16:00:28 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -1,66 +1,46 @@
+import streamlit as st
 import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from sklearn.linear_model import LinearRegression
 
-def load_csv():
-    path = input("Enter the path to the CSV file: ")
-    return pd.read_csv(path)
+st.title('Keyword Analyzer App')
 
+# Function to analyze common themes
 def analyze_common_themes(data):
     data['tokens'] = data['keywords'].apply(word_tokenize)
-
-    # Flatten list of tokens and calculate frequency
     all_tokens = [token for sublist in data['tokens'].tolist() for token in sublist]
     fdist = FreqDist(all_tokens)
-
-    # Consider top 10 common tokens as themes
     return [item[0] for item in fdist.most_common(10)]
 
+# Function to train prediction models
 def train_models(data, common_themes):
     for theme in common_themes:
         data[theme] = data['tokens'].apply(lambda x: 1 if theme in x else 0)
-
     model_search_volume = LinearRegression().fit(data[common_themes], data['search volume'])
     model_keyword_difficulty = LinearRegression().fit(data[common_themes], data['keyword difficulty'])
-
     return model_search_volume, model_keyword_difficulty
 
+# Function to make predictions
 def predict(data, common_themes, model_search_volume, model_keyword_difficulty):
     data['tokens'] = data['keywords'].apply(word_tokenize)
-
     for theme in common_themes:
         data[theme] = data['tokens'].apply(lambda x: 1 if theme in x else 0)
-
     data['predicted search volume'] = model_search_volume.predict(data[common_themes])
     data['predicted keyword difficulty'] = model_keyword_difficulty.predict(data[common_themes])
-
     return data
 
-def main():
-    print("Welcome to the Keyword Analyzer App!")
+uploaded_file = st.file_uploader("Choose your CSV file for analysis", type="csv")
 
-    # Step 1: Load first CSV and analyze
-    data = load_csv()
+if uploaded_file:
+    data = pd.read_csv(uploaded_file)
     common_themes = analyze_common_themes(data)
-    print(f"Common themes found: {', '.join(common_themes)}")
-
-    # Step 2: Train prediction models
+    st.write(f"Common themes found: {', '.join(common_themes)}")
     model_search_volume, model_keyword_difficulty = train_models(data, common_themes)
 
-    # Step 3: Load second CSV and predict
-    second_batch = load_csv()
-    predictions = predict(second_batch, common_themes, model_search_volume, model_keyword_difficulty)
-    
-    # Optionally, save the predictions to a CSV
-    save = input("Do you want to save the predictions to a CSV? (yes/no): ").strip().lower()
-    if save == 'yes':
-        output_path = input("Enter the path to save the CSV: ")
-        predictions.to_csv(output_path, index=False)
-        print(f"Predictions saved to {output_path}")
-
-    print("Thank you for using the Keyword Analyzer App!")
-
-if __name__ == "__main__":
-    main()
+    second_file = st.file_uploader("Choose your second CSV file for prediction", type="csv")
+    if second_file:
+        second_batch = pd.read_csv(second_file)
+        predictions = predict(second_batch, common_themes, model_search_volume, model_keyword_difficulty)
+        st.write(predictions)
